086d35a651153e8f7c1a0dd1b6eb3d4d33ec73be
shawcorey <shaw.corey@gmail.com>
2021-06-17 18:00:16 -0500

    updated additional functions

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -2,38 +2,39 @@ function addMovieListToHTML(movieData) {
     console.log(movieData)
     let movieList = $("#movie-list");
     movieList.html("")
-    movieData.forEach( currentMovie => {
+    movieData.forEach(currentMovie => {
         movieList.append(
             `<div id="container-id-${currentMovie.id}" data-id="${currentMovie.id}" class="row border">
-                <div class="col-6 p-1">Movie: ${currentMovie.title}</div>
-                <div class="col-6 p-1">Rating: ${currentMovie.rating}</div>
-                <div class="col-12 p-1">Plot: ${currentMovie.plot}</div>
-                <a href="#" class='col-3 edit-button'>Edit</a>
-                <a href="#"  class='col-3 delete-button' onclick="deleteButtonIsClicked" >Delete</a>
+                <input class="col-6 p-1">Movie: ${currentMovie.title}</input>
+                <input class="col-6 p-1">Rating: ${currentMovie.rating}</input>
+                <input class="col-12 p-1">Plot: ${currentMovie.plot}</input>
+                <button class="col-3 edit-button">Edit</button>
+                <button  class="col-3 delete-button" data-id="${currentMovie.id}" >Delete</button>
             </div>
             `
         )
     })
+    addDeleteBtnEvents()
 }
+
 //Un-hides Add a Movie Section after Load
-function toggleAddMovieForm () {
+function toggleAddMovieForm() {
     let addMovieSection = $('#add-movie-section')
     addMovieSection.toggleClass('toggle-add-movie')
 }
 
 let addMovieButton = $("#add-movie-button");
 addMovieButton.click(addNewMovieInfo)
-function createClickEvents() {
-    let deleteButtons = $(".delete-button");
-    deleteButtons.on("click", deleteButtonIsClicked);
+
+function addDeleteBtnEvents() {
+
+    $(".delete-button").on("click", function () {
+        console.log($(this).attr("data-id"));
+    });
 }
 
 
-function deleteButtonIsClicked(e) {
-    console.log("delete button was clicked");
-    let test = $(this);
-    console.log(test);
-}
+
 
 
 

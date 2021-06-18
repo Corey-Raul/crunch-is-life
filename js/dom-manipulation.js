6d52c5251b381bb6ab133e64f5c6238ebf5e8d50
shawcorey <shaw.corey@gmail.com>
2021-06-18 15:56:03 -0500

    updated site with cards

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -1,67 +1,71 @@
 function addMovieListToHTML(movieData) {
-	let movieList = $("#movie-list");
-	movieList.html("")
-	movieData.forEach(currentMovie => {
-		let id = currentMovie.id;
-		let title = currentMovie.title.toUpperCase();
-		let plot = currentMovie.plot;
-		let rating = currentMovie.rating
-		let genre = currentMovie.genre
-		let poster = currentMovie.poster
-		movieList.append(
-			//THERE ARE THE INDIVIDUAL MOVIE CARDS
-			`<div id="container-id-${id}" data-id="${id}" class="row border rounded movie-container p-3 my-2">
-				<img src="${poster}" alt="img-id-${id}" class="col-7 mx-auto">
-                <div id="title-id-${id}" class="col-8 p-1"><strong>Tittle: </strong>${title}</div>
-                <div id="rating-id-${id}" class="col-4 p-1"><strong>Rating: </strong>${rating}</div>
-                <div id="plot-id-${id}" class="col-12 p-1 plot-background"><strong>Plot:</strong><br> ${plot}</div>
-                <div id="genre-id-${id}" class="col-12 p-1 genre-background"><strong>Genre:</strong><br> ${genre}</div>
-                <div id="edit-section-id-${id}" class="col-12 toggle-feature"></div>
-                <button class="col-5 edit-button btn btn-light m-1 mx-auto" data-id="${id}">Edit</button>
-                <button  class="col-5 delete-button btn btn-light m-1 mx-auto" data-id="${id}" >Delete</button>
-            </div>`
-		)
-	})
+    let movieList = $("#movie-list");
+    movieList.html("")
+    movieData.forEach(currentMovie => {
+        let id = currentMovie.id;
+        let title = currentMovie.title.toUpperCase();
+        let plot = currentMovie.plot;
+        let rating = currentMovie.rating
+        let genre = currentMovie.genre
+        let poster = currentMovie.poster
+        movieList.append(
+            //THESE ARE THE INDIVIDUAL MOVIE CARDS
+            //EVERYTHING INSIDE OF THE TICKS `` IS BEING APPENDED
 
-	addDeleteBtnEvents()
-	//Add EditButtonEvent
-	addEditButtonEvents()
+            `<div class="col-12 col-sm-5 m-1 card">
+                <img src="${poster}" alt="img-id-${id}" class="card-img-top mx-auto">
+                <div class="card-body">
+                    <div id="title-id-${id}" class="col-8 p-1 card-title">${title}</div>
+                    <div id="rating-id-${id}" class="col-4 p-1 card-text"><strong>Rating: </strong>${rating}</div>
+                    <div id="plot-id-${id}" class="col-12 p-1 card-text plot-background"><strong>Plot:</strong><br> ${plot}</div>
+                    <div id="genre-id-${id}" class="col-12 p-1 card-text genre-background"><strong>Genre:</strong><br> ${genre}</div>
+                    <div id="edit-section-id-${id}" class="col-12 toggle-feature"></div>
+                    <button class="col-5 edit-button btn btn-light m-1 mx-auto" data-id="${id}">Edit</button>
+                    <button  class="col-5 delete-button btn btn-light m-1 mx-auto" data-id="${id}" >Delete</button>
+                </div>
+            </div>`
+        )
+    })
+
+    addDeleteBtnEvents()
+    //Add EditButtonEvent
+    addEditButtonEvents()
 
 }
 
 //Un-hides Add a Movie Section after Load
 function toggleAddMovieForm() {
-	console.log('TOGGLE FUNCTION')
-	let addMovieSection = $('#add-movie-section')
-	addMovieSection.toggleClass('toggle-feature')
+    console.log('TOGGLE FUNCTION')
+    let addMovieSection = $('#add-movie-section')
+    addMovieSection.toggleClass('toggle-feature')
 }
 
 let addMovieButton = $("#add-movie-button");
 addMovieButton.click(addNewMovieInfo)
 
 function addDeleteBtnEvents() {
-	$(".delete-button").on("click", function () {
-		let id = $(this).attr("data-id")
-		let confirm = window.confirm('Are you sure')
-		if (confirm) {
-			deleteMovie(id);
-		}
-	});
+    $(".delete-button").on("click", function () {
+        let id = $(this).attr("data-id")
+        let confirm = window.confirm('Are you sure')
+        if (confirm) {
+            deleteMovie(id);
+        }
+    });
 }
 
 function addEditButtonEvents() {
-	$('.edit-button').on('click', function () {
-		let id = $(this).parent().attr('data-id');
-		createEditingSection(id)
-	});
+    $('.edit-button').on('click', function () {
+        let id = $(this).parent().attr('data-id');
+        createEditingSection(id)
+    });
 }
 
 function createEditingSection(parentID) {
-	// parentID = $(this).parent().attr('data-id');
-	let editSection = $(`#edit-section-id-${parentID}`)
-	editSection.html('').toggleClass('toggle-feature')
-	let currentMovie = realMovieData[parentID - 1]
-	editSection.append(`
+    // parentID = $(this).parent().attr('data-id');
+    let editSection = $(`#edit-section-id-${parentID}`)
+    editSection.html('').toggleClass('toggle-feature')
+    let currentMovie = realMovieData[parentID - 1]
+    editSection.append(`
             <form class="row">
             	<div class="col-6">
             		<div class="row m-1">
@@ -110,20 +114,20 @@ function createEditingSection(parentID) {
 				</div>
                 <button id="edit-submit-button-${parentID}" data-id="${parentID}" type="submit" class="mx-auto col-11 btn btn-info submit-button m-1"><strong>Submit</strong></button> 
             </form>`
-	)
-	$('.submit-button').on('click', function (e) {
-		e.preventDefault();
-		console.log($('.submit-button'));
-		let id = $(this).attr('data-id');
-		submitForEditEvent(id);
-	})
+    )
+    $('.submit-button').on('click', function (e) {
+        e.preventDefault();
+        console.log($('.submit-button'));
+        let id = $(this).attr('data-id');
+        submitForEditEvent(id);
+    })
 }
 
 // if (currentMovie.name.toLowerCase().includes(inputSearch.toLowerCase()) ) {
 //	newMoviesArray.push(currentMovie)
 // }
-function sortTest (movieData) {
-	let jumbotronSection = $('')
+function sortTest(movieData) {
+    let jumbotronSection = $('')
 }
 
 // {
